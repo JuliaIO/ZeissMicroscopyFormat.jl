ea18f6e8ed4b4896adfb8e1c1287319d786cf4bb
Tim Holy <tim.holy@gmail.com>
2022-05-03 08:50:31 -0500

    Parse metadata & extract the layout (#2)

--- src/ZeissMicroscopyFormat.jl
+++ src/ZeissMicroscopyFormat.jl
@@ -1,14 +1,16 @@
 module ZeissMicroscopyFormat
 
 using Mmap
+using Dates
 
 using FileIO
-using TiffImages
 using OMETIFF
+using EzXML: EzXML, eachelement, firstelement, nextelement, nodename, nodecontent
 using FixedPointNumbers
 using ColorTypes
 using ImageMetadata
-using LazyStack
+using Unitful
+using BlockArrays
 
 # Documentation on this format is proprietary but available by request for free:
 #    https://www.zeiss.com/microscopy/int/products/microscope-software/zen/czi.html
@@ -34,6 +36,8 @@ const pixeltypes = Dict{Int32,DataType}(
     # unsupported: 10 => Gray{Complex{Float32}}, 11 => BGR{Complex{Float32}}
     12 => Gray{N0f32}
 )
+
+const zeissdtfmt = dateformat"y-m-dTH:M:S.s"
 
 struct DimensionEntryDV
     dimension::Char
@@ -69,13 +73,7 @@ struct DirectoryEntryDVFixed
     spare4::UInt32
     dimensioncount::Int32
 end
-
-struct DirectoryEntryDV
-    fixed::DirectoryEntryDVFixed
-    dimensionentries::Vector{DimensionEntryDV}
-end
-
-function DirectoryEntryDV(s::Stream)
+function DirectoryEntryDVFixed(s::Stream)
     c1, c2 = read(s, UInt8), read(s, UInt8)
     @assert Char(c1) == 'D' && Char(c2) == 'V'
     # fixed = reinterpret(DirectoryEntryDVFixed, read(s, sizeof(DirectoryEntryDVFixed)))[]
@@ -88,7 +86,21 @@ function DirectoryEntryDV(s::Stream)
                                   read(s, UInt32),   # spare4
                                   read(s, Int32),
     )
-    @assert iszero(fixed.compression)   # FIXME: support compression
+    # The following constraint comes from the fact that the size of the `Fill` section
+    # of SubBlockSegments is `max(256-n, 0)` where `n = dimensioncount*20 + 28 + 16`
+    # and here we've hard-coded it to be the first of these.
+    @assert fixed.dimensioncount <= 10 "FIXME: more than 10 dimensions unsupported"
+    return fixed
+end
+
+struct DirectoryEntryDV
+    fixed::DirectoryEntryDVFixed
+    dimensionentries::Vector{DimensionEntryDV}
+end
+function DirectoryEntryDV(s::Stream)
+    fixed = DirectoryEntryDVFixed(s)
+    @assert iszero(fixed.compression) "FIXME: support compression"
+    @assert iszero(fixed.pyramidtype) "FIXME: support pyramids"
     # entries = reinterpret(DimensionEntryDV, read(s, fixed.dimensioncount * sizeof(DimensionEntryDV)))
     entries = [DimensionEntryDV(s) for _ = 1:fixed.dimensioncount]
     return DirectoryEntryDV(fixed, entries)
@@ -110,19 +122,33 @@ struct SubBlockSegmentSizes
     attachsize::Int32
     datasize::Int64
 end
-struct DataSegment{C<:Colorant,N}
-    offset::Int64
-    dims::NTuple{N,Int}
-    dimnames::NTuple{N,Char}
-    metadata
-end
+SubBlockSegmentSizes(s::Stream) = SubBlockSegmentSizes(read(s, Int32), read(s, Int32), read(s, Int64))
 
-function read_subblock(s::Stream, d::DirectoryEntryDV) where C<:Colorant
+struct SubBlock{C<:Colorant,N}
+    sbss::SubBlockSegmentSizes
+    d::DirectoryEntryDV
+    metadataoffset::Int64
+    dataoffset::Int64
+    dims::NTuple{N,Int}
+    dimnames::NTuple{N,Symbol}
+end
+function SubBlock{C,N}(s::Stream, pos::Integer, sbss::SubBlockSegmentSizes, d::DirectoryEntryDV) where {C<:Colorant,N}
+    mdpos = Int(pos) + 256
+    return SubBlock{C,N}(sbss, d, mdpos, mdpos + sbss.metasize, size(d)::NTuple{N,Int}, names(d)::NTuple{N,Symbol})
+end
+function SubBlock{C,N}(s::Stream, d::DirectoryEntryDV) where {C<:Colorant,N}
+    seek(s, d.fileposition)
+    sid = read(s, 16)
+    @assert sid == SUBBLOCK
+    read(s, 16)
+    sbss = SubBlockSegmentSizes(s)
+    return SubBlock{C,N}(s, d.fileposition + 32, sbss, d)
+end
+function SubBlock(s::Stream)
     pos = position(s)
-    sbss = reinterpret(SubBlockSegmentSizes, read(s, sizeof(SubBlockSegmentSizes)))[1]
-    seek(s, pos+256)
-    md = parsexml(String(read(s, sbss.metasize)))
-    return DataSegment{pixeltypes[d.pixeltype],Int(d.dimensioncount)}(position(s), )
+    sbss = SubBlockSegmentSizes(s)
+    d = DirectoryEntryDV(s)
+    return SubBlock{pixeltypes[d.pixeltype],Int(d.dimensioncount)}(s, pos, sbss, d)
 end
 
 function load(f::File{format"CZI"})
@@ -164,9 +190,9 @@ function load(s::Stream{format"CZI"}; keywords...)
     # if idx !== nothing
     #     rawxml = rawxml[1:prevind(rawxml, idx)]
     # end
-    omexml = OMETIFF.root(OMETIFF.parsexml(rawxml))
+    omexml = EzXML.root(EzXML.parsexml(rawxml))
 
-    # Subblocks
+    # Subblock directory
     seek(s, dirpos)
     sid = read(s, 16)
     @assert sid == RAWDIR
@@ -175,41 +201,117 @@ function load(s::Stream{format"CZI"}; keywords...)
     seek(s, dirpos + 128 + 32)
     entries = [DirectoryEntryDV(s) for i = 1:entry_count]
     pixeltype = first(entries).pixeltype
-    nd = first(entries).dimensioncount
+    T = pixeltypes[pixeltype]
+    nd = Int(first(entries).dimensioncount)
     sz = size(first(entries))
-    @show names(first(entries))
     # @show sz map(size, entries)
     @assert all(d -> d.pixeltype == pixeltype, entries) "All pixel types must be identical"
     @assert all(d -> d.dimensioncount == nd, entries) "The number of dimensions must be consistent"
     @assert all(d -> size(d)[1:end-1] == sz[1:end-1], entries) "Leading sizes must be identical"
+    # Subblocks (mostly to parse the XML)
+    subblocks = [SubBlock{T,nd}(s, entry) for entry in entries]
+
+    nb = sizeof(T)::Int
+    @assert all(subblock -> iszero(subblock.dataoffset % nb), subblocks) "FIXME: unaligned chunk boundaries are not yet supported"
 
     # Data
     # FIXME? To avoid another layer of `ReinterpretArray`, it's easiest to interpret the raw data
     # as having the eltype of the image. However, it's possible the chunk boundaries will not
-    # always be aligned commensurately. For now let's run with this, but it may require generalization.
-    T = pixeltypes[pixeltype]
-    nb = sizeof(T)::Int
-    @assert all(d -> iszero(d.fileposition % nb), entries) "FIXME: chunk boundaries are not aligned"
+    # always be aligned commensurately. For now, let's assume it's OK, but it may require generalization.
+    C, axs, shear = layout(omexml, T)
+    @show C axs shear
     seek(s, 0)
     mm = Mmap.mmap(s.io, Vector{T}, filesize(s.io) ÷ nb)
-    blocks = [makeview(mm, entry, nb) for entry in entries]
+    blocks = [makeview(mm, subblock, nb) for subblock in subblocks]
+    subblock = first(subblocks)
+    blocked = mortar(reshape(blocks, 1, 1, map(name -> length(axs[name]), subblock.dimnames[3:end])...))
 
-    return ImageMeta(stack(blocks); xml=omexml, entries)  # FIXME
+    return ImageMeta(blocked; xml=omexml, subblocks, shear, suppress=Set([:xml, :subblocks]))
 end
 
-function makeview(v, entry, nb)
-    start = entry.fileposition ÷ nb
-    sz = size(entry)
+function makeview(v, subblock::SubBlock, nb)
+    start = subblock.dataoffset ÷ nb
+    sz = size(subblock.d)
     nel = prod(sz)
-    # Drop the color channel ('C') if eltype(v) <: Gray
-    if eltype(v) <: AbstractGray
-        nms = names(entry)
-        idx = findfirst(==(:C), nms)
-        if idx !== nothing
-            sz = (sz[1:idx-1]..., sz[idx+1:end]...)
+    # # Drop the color channel ('C') if eltype(v) <: Gray
+    # if eltype(v) <: AbstractGray
+    #     nms = names(subblock.d)
+    #     idx = findfirst(==(:C), nms)
+    #     if idx !== nothing
+    #         sz = (sz[1:idx-1]..., sz[idx+1:end]...)
+    #     end
+    # end
+    return reshape(view(v, start:start+nel-1), sz)
+end
+
+function layout(omexml, ::Type{T}) where T
+    imagenode = only(filter!(node -> !isempty(eachelement(node)), findall("//Image", omexml)))
+    axs = Dict{Symbol,Any}()
+    szs = Dict{Symbol,Int}()
+    wavelengths = Float32[]
+    shear = nothing
+    for node in eachelement(imagenode)
+        nn = nodename(node)
+        if startswith(nn, "Size")
+            sym = Symbol(nn[end])
+            szs[sym] = parse(Int, nodecontent(node))
+        elseif nn == "Dimensions"
+            for subnode in eachelement(node)
+                nn = nodename(subnode)
+                if nn == "Channels"
+                    for cnode in eachelement(subnode)
+                        for anode in eachelement(cnode)
+                            nn = nodename(anode)
+                            if nn == "EmissionWavelength"
+                                push!(wavelengths, parse(Float32, nodecontent(anode)))
+                            end
+                        end
+                    end
+                elseif nn == "T"
+                    starttime, start, increment = nothing, nothing, nothing
+                    for anode in eachelement(subnode)
+                        nn = nodename(anode)
+                        if nn == "StartTime"
+                            starttime = nodecontent(anode)
+                        elseif nn == "Positions"
+                            start, increment = parse_positions(anode)
+                        end
+                    end
+                    idx = findfirst('.', starttime)
+                    starttime = DateTime(starttime[1:idx+3], zeissdtfmt)   # TODO: do something with this?
+                    axs[:T] = range(start * u"s", step=increment * u"s", length=szs[:T])
+                elseif nn == "Z"
+                    start, increment = nothing, nothing
+                    for anode in eachelement(subnode)
+                        nn = nodename(anode)
+                        if nn == "ZAxisShear"
+                            shear = nodecontent(anode)
+                        elseif nn == "Positions"
+                            start, increment = parse_positions(anode)
+                        end
+                    end
+                    axs[:Z] = range(start * u"μm", step=increment * u"μm", length=szs[:Z])
+                end
+            end
         end
     end
-    return reshape(view(v, start:start+nel-1), sz)
+    pixelsizenode = only(findall("//ImagePixelSize", omexml))
+    yinc, xinc = parse.(Float64, split(nodecontent(pixelsizenode), ','))
+    axs[:C] = 1:length(wavelengths)
+    axs[:Y] = range(0 * u"μm", step=yinc * u"μm", length=szs[:Y])
+    axs[:X] = range(0 * u"μm", step=xinc * u"μm", length=szs[:X])
+    return wavelengths, axs, shear
+end
+
+function parse_positions(node)
+    node = only(eachelement(node))  # Interval
+    startnode = firstelement(node)
+    @assert nodename(startnode) == "Start"
+    start = nodecontent(startnode)
+    incnode = nextelement(startnode)
+    @assert nodename(incnode) == "Increment"
+    increment = nodecontent(incnode)
+    return parse(Float64, start), parse(Float64, increment)
 end
 
 end
